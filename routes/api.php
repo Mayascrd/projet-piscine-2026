e5465927f85fdd3894e2b2cb5d6dad9471fe985e
Mayascrd <Maya.sicard@edu.ece.fr>
2026-05-31 21:10:40 +0200

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+require_once __DIR__ . '/../controllers/AuthController.php';
+
+$db = (new Database())->connect();
+
+$authController = new AuthController($db);
+
+$uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$method = $_SERVER["REQUEST_METHOD"];
+
+switch ($uri)
+{
+    case "/api/register":
+
+        if($method === "POST"){
+            $authController->register();
+        }
+
+        break;
+
+    case "/api/login":
+
+        if($method === "POST"){
+            $authController->login();
+        }
+
+        break;
+
+    case "/api/logout":
+
+        if($method === "POST"){
+            $authController->logout();
+        }
+
+        break;
+
+    default:
+
+        http_response_code(404);
+
+        echo json_encode([
+            "message" => "Route introuvable"
+        ]);
+}
